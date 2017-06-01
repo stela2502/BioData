d28eb5780a688206d9e6bfc95950f2f3e74bc386
stefanl <stefanl@aurora-rviz02.int>
2017-06-01 10:14:39 +0200

    CreateStats for the BioData object using DEseq

--- R/createStats.R
+++ R/createStats.R
@@ -18,42 +18,42 @@ setGeneric('createStats', ## Name
 	}
 )
 #
-#setMethod('createStats', signature = c ('NGSexpressionSet'),
-#	definition = function (x, condition, files=F, A=NULL, B=NULL) {
-#		#stop( "Not implemented / broken!")
-#		
-#		if ( nrow(x$data) < 1e+3 ) {
-#			stop ( "Please calculate the statistics only for the whole dataset!" )
-#		}
-#		if ( is.na( match ( condition, colnames(x$samples))) ) {
-#			stop ( 'Please select a condition from the sample colnames' )
-#		}
-#		id <- match ( condition, names(x$usedObj[['cds']]))
-#		if ( is.na(id)) { id <- 1 } 
-#		x<- preprocess ( x, condition )
-#		conditions <- as.vector(unique(x$samples[,condition]))
-#		if ( ! is.null(A) && ! is.null(B)) {
-#			x <- add_to_stat ( x, 
-#				stat = DESeq::nbinomTest(x$usedObj[['cds']][[id]], A, B ), 
-#				name = paste( A, B ,sep=' vs. ')
-#			)
-#		}
-#		else {
-#			for ( i in 1:(length(conditions)-1) ){
-#				for ( a in (i+1):length(conditions) ){
-#					x <- add_to_stat ( x, 
-#						stat = DESeq::nbinomTest(x$usedObj[['cds']][[id]], conditions[i] , conditions[a] ), 
-#						name = paste( conditions[i], conditions[a],sep=' vs. ')
-#					)
-#
-#				}
-#			}
-#		}
-#		if ( files ) {
-#			writeStatTables( x )
-#		}
-#		x
-#})
+setMethod('createStats', signature = c ('BioData'),
+	definition = function (x, condition, files=F, A=NULL, B=NULL) {
+		#stop( "Not implemented / broken!")
+		
+		if ( nrow(x$data) < 1e+3 ) {
+			stop ( "Please calculate the statistics only for the whole dataset!" )
+		}
+		if ( is.na( match ( condition, colnames(x$samples))) ) {
+			stop ( 'Please select a condition from the sample colnames' )
+		}
+		id <- match ( condition, names(x$usedObj[['cds']]))
+		if ( is.na(id)) { id <- 1 } 
+		x<- preprocess ( x, condition )
+		conditions <- as.vector(unique(x$samples[,condition]))
+		if ( ! is.null(A) && ! is.null(B)) {
+			x <- add_to_stat ( x, 
+				stat = DESeq::nbinomTest(x$usedObj[['cds']][[id]], A, B ), 
+				name = paste( A, B ,sep=' vs. ')
+			)
+		}
+		else {
+			for ( i in 1:(length(conditions)-1) ){
+				for ( a in (i+1):length(conditions) ){
+					x <- add_to_stat ( x, 
+						stat = DESeq::nbinomTest(x$usedObj[['cds']][[id]], conditions[i] , conditions[a] ), 
+						name = paste( conditions[i], conditions[a],sep=' vs. ')
+					)
+
+				}
+			}
+		}
+		if ( files ) {
+			writeStatTables( x )
+		}
+		x
+})
 
 add_to_stat <- function( x, stat, name ) {
 	if ( ! is.na( match( name, names(x$stats)))){

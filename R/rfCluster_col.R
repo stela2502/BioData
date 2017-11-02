8ba3f1a93218c236f870dc368bba6b4b69cd58f5
stefanlang <Stefan.Lang@med.lu.se>
2017-11-02 09:35:13 +0100

    rfCluster_row fixed same bug as ever.

--- R/rfCluster_col.R
+++ R/rfCluster_col.R
@@ -76,9 +76,16 @@ setMethod('rfCluster_col', signature = c ('BioData'),
 						x$usedObj[['rfExpressionSets']][[ i ]] <- reduceTo( x, what='col', to=colnames(x$dat)[sample(c(1:total),total-subset)], name=tname, copy=TRUE )
 						if ( length(settings) > 0 ) {
 							#browser()
-							x$usedObj[['rfObj']][[ i ]] <- RFclust.SGE::RFclust.SGE ( dat=as.data.frame(x$usedObj[['rfExpressionSets']][[ i ]]$data()), SGE=F, slices=slice, email=email, tmp.path=opath, name= tname, settings=settings, slurm=T )
+							x$usedObj[['rfObj']][[ i ]] <- RFclust.SGE::RFclust.SGE ( 
+									dat=as.data.frame(x$usedObj[['rfExpressionSets']][[ i ]]$data()), 
+									SGE=F, slices=slice, email=email, tmp.path=opath, 
+									name= tname, settings=settings, slurm=T 
+							)
 						}else {
-							x$usedObj[['rfObj']][[ i ]] <- RFclust.SGE::RFclust.SGE ( dat=as.data.frame(x$usedObj[['rfExpressionSets']][[ i ]]$data()), SGE=SGE, slices=slice, email=email, tmp.path=opath, name= tname )
+							x$usedObj[['rfObj']][[ i ]] <- RFclust.SGE::RFclust.SGE ( 
+									dat=as.data.frame(x$usedObj[['rfExpressionSets']][[ i ]]$data()), 
+									SGE=SGE, slices=slice, email=email, tmp.path=opath, name= tname 
+							)
 						}
 					}
 					names(x$usedObj[['rfExpressionSets']]) [i] <- tname

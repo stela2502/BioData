b0328c287a2c5e892fd8f57aeeb380d4a1e2a592
Stefan Lang <Stefan.Lang@med.lu.se>
2017-11-24 12:02:03 +0100

    as_BioData now converts Seurat objects too.

--- R/rfCluster_col.R
+++ R/rfCluster_col.R
@@ -13,9 +13,8 @@
 #' @param x the single cells ngs object
 #' @param email your email to use together with the SGE option
 #' @param SGE whether to use the sun grid engine to calculate the rf grouping
-#' @param rep how many repetitions for the random forest grouping should be run (default = 5)
-#' @param slice how many processes should be started for each random forest clustering (default = 30)
-#' @param bestColname the column name to store the results in
+#' @param rep how many repetitions for the random forest grouping should be run (default = 1)
+#' @param slice how many processes should be started for each random forest clustering (default = 4)
 #' @param k the numer of expected clusters (better more than to view)
 #' @param subset how many cells should be randomly selected for the unsupervised clustering (default = 200)
 #' @param name if you want to run multiple RFclusterings on e.g. using different input genes you need to specify a name (default ='RFclust')

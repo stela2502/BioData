bfad6b090fbb34e4b8612790979773d7b7bedc54
stefanlang <Stefan.Lang@med.lu.se>
2018-10-31 09:22:51 +0100

    finally fixed after the merge chaos

--- R/mds.R
+++ R/mds.R
@@ -60,10 +60,15 @@ setMethod('mds', signature = c ('BioData'),
 					
 					if ( nrow(dataObj$annotation) > 2000 ) {
 						message ( "irlba::prcomp_irlba is used to save memory and time (more than 2000 genes)" )
-						dataObj$usedObj$pr <- irlba::prcomp_irlba ( t(tmp), center=T, n=n )
+						dataObj$usedObj$pr <- irlba::prcomp_irlba ( t(tmp), center=T, n=n+1 )
+						#dataObj$usedObj$pr$x <- apply(dataObj$usedObj$pr$x ,2, function(x) { resid( lm(x ~ dataObj$usedObj$pr$x[,2] ) ) } )
+						#dataObj$usedObj$pr$x <- dataObj$usedObj$pr$x[,-2]
 						rownames(dataObj$usedObj$pr$x) = colnames(dataObj$dat)
+						
 					}else {
-						dataObj$usedObj$pr <- pcaMethods::bpca( t(as.matrix(tmp)), nPcs=100 )
+						dataObj$usedObj$pr <- pcaMethods::bpca( t(as.matrix(tmp)), nPcs=n+1 )
+						#dataObj$usedObj$pr@scores <- apply( dataObj$usedObj$pr@scores,2, function(x) { resid( lm(x ~ dataObj$usedObj$pr@scores[,2] ) ) } )
+						#dataObj$usedObj$pr@scores <- dataObj$usedObj$pr@scores[,-2]
 						rownames(dataObj$usedObj$pr@scores) = colnames(dataObj$dat)
 					}
 					rm(tmp)
@@ -126,143 +131,140 @@ setMethod('mds', signature = c ('BioData'),
 				
 				if(mds.type == "PCA"){
 					mds.proj <- tab[,1:dim]
-					try( {
-								png ( file=file.path( dataObj$outpath,'loadings.png'), width=1000, height=1000 )
-								plot (  pr$rotation[,1:2] , col='white' );
-								text( pr$rotation[,1:2], labels= rownames(pr$rotation), cex=1.5 )
-								dev.off()
-								write.table( cbind( Genes = rownames(pr$rotation), pr$rotation[,1:2] ), 
-										file=file.path( dataObj$outpath,'gene_loadings.xls') , row.names=F, sep='\t',quote=F )
-							})
 				}
 				#	mds.trans <- prcomp(t(tab))$x[,1:3]
 				
-			} else if ( mds.type=='DM') {
-				if (!library("destiny", quietly = TRUE,logical.return=TRUE )) {
-					stop("package 'destiny' needed for this function to work. Please install it.",
-							call. = FALSE)
-				}
-				if ( ! exists('sigma', mode='numeric') ){
-					if ( exists('find_sigmas', where='package:destiny', mode='function') ){
-						sigmas <- destiny::find_sigmas(tab, verbose=F)
-						sigma <- destiny::optimal_sigma(sigmas)
+				else if ( mds.type=='DM') {
+					if (!library("destiny", quietly = TRUE,logical.return=TRUE )) {
+						stop("package 'destiny' needed for this function to work. Please install it.",
+								call. = FALSE)
+					}
+					if ( ! exists('sigma', mode='numeric') ){
+						if ( exists('find_sigmas', where='package:destiny', mode='function') ){
+							sigmas <- destiny::find_sigmas(tab, verbose=F)
+							sigma <- destiny::optimal_sigma(sigmas)
+						}else {
+							sigmas <- destiny::find.sigmas(tab, verbose=F)
+							sigma <- destiny::optimal.sigma(sigmas)
+						}
+						
+					}
+					if ( !exists('distance', mode='character')){
+						distance = 'cosine'
+					}
+					dm <- destiny::DiffusionMap(tab, distance = distance, sigma = sigma, n_eigs = dim)
+					mds.proj <- destiny::as.data.frame(dm)[,1:dim]
+					
+				}else if ( mds.type == "TSNE"){
+					browser()
+					## there is an extremely efficient python implementation of that available.
+					## lets try to use that:
+					## first export the '\t' separated file
+					if ( file.exists( 'runTSNE.py' ) & ! file.exists( 'tSNE_dim3_coods.csv') ){
+						print ( "The pyton process should be started and processing the data - please check manually")
+						return (invisible(dataObj))
+						
+					}else if ( ! file.exists( 'tSNE_dim3_coods.csv' ) )  {
+						
+						write.table( t(tab), sep=",", file="TSNE_data.csv", quote=F )
+						fileConn<-file( 'runTSNE.py' )
+						writeLines(c(
+										"from MulticoreTSNE import MulticoreTSNE as TSNE",
+										"import pandas as pd",
+										"import numpy as np",
+										"",
+										paste( sep="","tsne = TSNE(n_jobs=4,n_components=",dim,")"),
+										"X = pd.io.parsers.read_csv('TSNE_data.csv',sep=',',index_col=0)",
+										"",
+										"Y = tsne.fit_transform(np.transpose(X))",
+										"",
+										"df=pd.DataFrame(data=Y,index=None)",
+										"df.to_csv('tSNE_dim3_coods.csv')"
+								), fileConn )
+						close(fileConn)
+						if (! is.null(pythonEnv)) {
+							system( paste( pythonEnv ,"&&", 'python' , 'runTSNE.py' ) )					
+						}else {
+							system( paste(Sys.which('python'), 'runTSNE.py' ) )
+						}
+						print ("the external python script has been run - rerun this function to check if it is finished.")
+						print ( "in case the python script does not produce output (1) try to install MulticoreTSNE grom its git resource or (2) use mds.type='TSNE_R'")
+						return (invisible(dataObj))
+					} else {
+						## OK the output file has been produced
+						mds.proj <- read.delim( 'tSNE_dim3_coods.csv', sep="," );
+						rownames(mds.proj) <- rownames(tab)
+					}
+				}else if ( mds.type == "TSNE_R"){
+					if (!library("Rtsne", quietly = TRUE,logical.return=TRUE )) {
+						stop("package 'Rtsne' needed for this function to work. Please install it.",
+								call. = FALSE)
+					}
+					if ( useRaw ){
+						mds.proj <- Rtsne( tab, dims=dim , check_duplicates =F, pca_center=F, verbose=T, pca=T )$Y
 					}else {
-						sigmas <- destiny::find.sigmas(tab, verbose=F)
-						sigma <- destiny::optimal.sigma(sigmas)
+						## The data is already PCAed
+						mds.proj <- Rtsne( tab, dims=dim , check_duplicates =F,  verbose=T, pca=F )$Y
 					}
 					
-				}
-				if ( !exists('distance', mode='character')){
-					distance = 'cosine'
-				}
-				dm <- destiny::DiffusionMap(tab, distance = distance, sigma = sigma, n_eigs = dim)
-				mds.proj <- destiny::as.data.frame(dm)[,1:dim]
-				
-			}else if ( mds.type == "TSNE"){
-				
-				## there is an extremely efficient python implementation of that available.
-				## lets try to use that:
-				## first export the '\t' separated file
-				if ( file.exists( 'runTSNE.py' ) & ! file.exists( 'tSNE_dim3_coods.csv') ){
-					print ( "The pyton process should be started and processing the data - please check manually")
-					return (invisible(dataObj))
-					
-				}else if ( ! file.exists( 'tSNE_dim3_coods.csv' ) )  {
-					
-					write.table( t(tab), sep=",", file="TSNE_data.csv", quote=F )
-					fileConn<-file( 'runTSNE.py' )
-					writeLines(c(
-									"from MulticoreTSNE import MulticoreTSNE as TSNE",
-									"import pandas as pd",
-									"import numpy as np",
-									"",
-									paste( sep="","tsne = TSNE(n_jobs=4,n_components=",dim,")"),
-									"X = pd.io.parsers.read_csv('TSNE_data.csv',sep=',',index_col=0)",
-									"",
-									"Y = tsne.fit_transform(np.transpose(X))",
-									"",
-									"df=pd.DataFrame(data=Y,index=None)",
-									"df.to_csv('tSNE_dim3_coods.csv')"
-							), fileConn )
-					close(fileConn)
-					if (! is.null(pythonEnv)) {
-						system( paste( pythonEnv ,"&&", 'python' , 'runTSNE.py' ) )					
-					}else {
-						system( paste(Sys.which('python'), 'runTSNE.py' ) )
-					}
-					print ("the external python script has been run - rerun this function to check if it is finished.")
-					print ( "in case the python script does not produce output (1) try to install MulticoreTSNE grom its git resource or (2) use mds.type='TSNE_R'")
-					return (invisible(dataObj))
-				} else {
-					## OK the output file has been produced
-					mds.proj <- read.delim( 'tSNE_dim3_coods.csv', sep="," );
 					rownames(mds.proj) <- rownames(tab)
-				}
-			}else if ( mds.type == "TSNE_R"){
-				if (!library("Rtsne", quietly = TRUE,logical.return=TRUE )) {
-					stop("package 'Rtsne' needed for this function to work. Please install it.",
-							call. = FALSE)
-				}
-				if ( useRaw ){
-					mds.proj <- Rtsne( tab, dims=dim , check_duplicates =F, pca_center=F, verbose=T, pca=T )$Y
-				}else {
-					## The data is already PCAed
-					mds.proj <- Rtsne( tab, dims=dim , check_duplicates =F,  verbose=T, pca=F )$Y
+					
 				}
 				
-				rownames(mds.proj) <- rownames(tab)
-				
-			}
-			
-			else if ( mds.type == "LLE"){
-				mds.proj <- LLE( tab, dim = dim, k = as.numeric(LLEK) )
-				#	mds.trans <- LLE( t(tab), dim = 3, k = as.numeric(LLEK) )
-				
-			}else if ( mds.type == "ISOMAP"){
-				mds.proj <- Isomap( tab, dim = dim, k = as.numeric(LLEK) )$dim3
-				#	mds.trans <- Isomap( t(tab), dim = 3, k = as.numeric(LLEK) )$dim3
-				
-			}else if ( mds.type == "ZIFA" ) {
-				#stop( "Sorry ZIFA has to be double checked - are you working on normalized data - than ZIFA can not be applied!")
-				print ( "Running external python script to apply ZIFA dimensional reduction (Expression data only)" )
-				if ( genes ) {
-					write.table( dataObj$dat, file="ZIFA_input.dat", sep=" ", col.names=F, row.names=F , quote=F)
-				}else {
-					write.table( t(dataObj$dat), file="ZIFA_input.dat", sep=" ", col.names=F, row.names=F , quote=F)
+				else if ( mds.type == "LLE"){
+					mds.proj <- LLE( tab, dim = dim, k = as.numeric(LLEK) )
+					#	mds.trans <- LLE( t(tab), dim = 3, k = as.numeric(LLEK) )
+					
+				}else if ( mds.type == "ISOMAP"){
+					mds.proj <- Isomap( tab, dim = dim, k = as.numeric(LLEK) )$dim3
+					#	mds.trans <- Isomap( t(tab), dim = 3, k = as.numeric(LLEK) )$dim3
+					
+				}else if ( mds.type == "ZIFA" ) {
+					#stop( "Sorry ZIFA has to be double checked - are you working on normalized data - than ZIFA can not be applied!")
+					print ( "Running external python script to apply ZIFA dimensional reduction (Expression data only)" )
+					if ( genes ) {
+						write.table( dataObj$dat, file="ZIFA_input.dat", sep=" ", col.names=F, row.names=F , quote=F)
+					}else {
+						write.table( t(dataObj$dat), file="ZIFA_input.dat", sep=" ", col.names=F, row.names=F , quote=F)
+					}
+					write( c("from ZIFA import ZIFA","from ZIFA import block_ZIFA", "import numpy as np",
+									"Y = np.loadtxt('ZIFA_input.dat')", "Z, model_params = ZIFA.fitModel( Y, 3 )", 
+									"np.savetxt('TheMDS_ZIFA.xls', Z )" ), 
+							file= 'ZIFA_calc.py' )
+					system( "python ZIFA_calc.py" )
+					Sys.sleep(5)
+					mds.proj <- read.delim( "TheMDS_ZIFA.xls", sep=' ', header=F)
+					if ( genes ) {
+						rownames(mds.proj) <- rownames(dataObj$dat)
+					}else {
+						rownames(mds.proj) <- colnames(dataObj$dat)
+					}
+					colnames(mds.proj) <- c( 'x','y','z')
+					
+				} else if ( mds.type == "DDRTree" ) {
+					DDRTree_res <- DDRTree( t(tab), dimensions=3)
+					mds.proj <- t(DDRTree_res$Z)
+					rownames(mds.proj) <- rownames(tab)
+					dataObj$usedObj$DRRTree.genes <- DDRTree_res
+					
 				}
-				write( c("from ZIFA import ZIFA","from ZIFA import block_ZIFA", "import numpy as np",
-								"Y = np.loadtxt('ZIFA_input.dat')", "Z, model_params = ZIFA.fitModel( Y, 3 )", 
-								"np.savetxt('TheMDS_ZIFA.xls', Z )" ), 
-						file= 'ZIFA_calc.py' )
-				system( "python ZIFA_calc.py" )
-				Sys.sleep(5)
-				mds.proj <- read.delim( "TheMDS_ZIFA.xls", sep=' ', header=F)
+				else {
+					print( paste("Sory I can not work on the mds.type option",mds.type) )
+				}
 				if ( genes ) {
 					rownames(mds.proj) <- rownames(dataObj$dat)
-				}else {
-					rownames(mds.proj) <- colnames(dataObj$dat)
+				}else{
+					if ( is.null(mds.proj) ) {
+						print ("An error has occured - mds.proj == NULL\n")
+						browser()
+					}
+					
+					rownames(mds.proj) <- make.names(colnames(dataObj$dat))
 				}
-				colnames(mds.proj) <- c( 'x','y','z')
 				
-			} else if ( mds.type == "DDRTree" ) {
-				DDRTree_res <- DDRTree( t(tab), dimensions=3)
-				mds.proj <- t(DDRTree_res$Z)
-				rownames(mds.proj) <- rownames(tab)
-				dataObj$usedObj$DRRTree.genes <- DDRTree_res
-				
+				dataObj$usedObj[[mds_store]][[this.k]]<- mds.proj
 			}
-			else {
-				print( paste("Sory I can not work on the mds.type option",mds.type) )
-			}
-			if ( genes ) {
-				rownames(mds.proj) <- rownames(dataObj$dat)
-			}else{
-				rownames(mds.proj) <- make.names(colnames(dataObj$dat))
-			}
-			
-			dataObj$usedObj[[mds_store]][[this.k]]<- mds.proj
-			
 			
 			invisible(dataObj)
 		} 
-		)
+)

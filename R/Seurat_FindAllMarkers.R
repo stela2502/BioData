b26b8fcae0b065970dca63b85242520509e26658
stela2502 <Stefan.Lang@med.lu.se>
2019-03-18 11:57:36 +0100

    Seurat find all markers uses the c++ wilcox test and works.

--- R/Seurat_FindAllMarkers.R
+++ R/Seurat_FindAllMarkers.R
@@ -5,16 +5,18 @@
 #' @description  Use Seurat's FindAllMarkers function
 #' @param x the BioData object
 #' @param condition The grouping you wat to analyze
+#' @param logfc.threshold test only genes with a liog fold change of default=1.0 or more 
+#' @param minPct test only genes with expression in more than default=0.1 fraction of cells in at least one group
 #' @title description of function Seurat_FindAllMarkers
 #' @export 
 setGeneric('Seurat_FindAllMarkers', ## Name
-	function ( x , condition) { 
+	function ( x , condition, logfc.threshold=1.0, minPct=0.1) { 
 		standardGeneric('Seurat_FindAllMarkers')
 	}
 )
 
 setMethod('Seurat_FindAllMarkers', signature = c ('BioData'),
-	definition = function ( x , condition) {
+	definition = function ( x , condition, logfc.threshold=1.0, minPct=0.1) {
 	
 	
 	#object <- as_Seurat( x, group=condition, fromRaw=F ) ## I want to use MY data
@@ -22,13 +24,16 @@ setMethod('Seurat_FindAllMarkers', signature = c ('BioData'),
 	
 			
 	CppStats <- function( n ) {
+		print ( paste("Calc wilcox test for",n) )
 		OK = which(grp.vec == n )
 		BAD= which(grp.vec != n )
-		r = as.data.frame(
-				FastWilcoxTest::StatTest( Matrix::t( x@dat), OK, BAD, logfc.threshold, minPct )
-		)
-		r= r[order( r[,'p.value']),]
-		r = cbind( r, cluster= rep(n,nrow(r) ), gene=rownames(x@dat)[r[,1]] )
+		if ( length(OK) > 0 && length(BAD) > 0 ){
+			r = as.data.frame(
+					FastWilcoxTest::StatTest( Matrix::t( x$dat), OK, BAD, logfc.threshold, minPct )
+			)
+			r= r[order( r[,'p.value']),]
+			r = cbind( r, cluster= rep(n,nrow(r) ), gene=rownames(x$dat)[r[,1]] )
+		}
 		r
 	}
 	
@@ -36,7 +41,7 @@ setMethod('Seurat_FindAllMarkers', signature = c ('BioData'),
 	grp.vec = as.vector(x$samples[,condition])
 	
 	for ( n in  unique( sort(grp.vec)) ) {
-		stats = rbind( all_markers, CppStats(n) )
+		stats = rbind( stats, CppStats(n) )
 	}
 	
 	

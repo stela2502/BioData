7fbf471bd3c63c76719ed5eb85600ea31e258f9a
stefanlang <Stefan.Lang@med.lu.se>
2019-06-25 08:35:51 +0200

    Reorder now also reorderes normal vectors.

--- R/reorder_grouping.R
+++ R/reorder_grouping.R
@@ -18,14 +18,20 @@ setGeneric('reorder_grouping', ## Name
 setMethod('reorder_grouping', signature = c ('BioData'),
 	definition = function (obj, group, new_order, what='col' ) {
 		if ( what == 'col'){
+			if ( ! is.factor(obj$samples[,group])){
+				obj$samples[,group] = factor( obj$samples[,group] )
+			}
 			k <- length(levels(obj$samples[,group]) )
 			err = NULL
-			if ( ! all.equal( sort(as.character(new_order)), sort(levels(obj$samples[,group])) ) == T ) {
+			if ( ! all.equal( sort(as.character(new_order)), sort(levels(obj$samples[,group])) ) == T ){ 
 				stop( paste("Missing/duplicated levels!", paste(sort(new_order), collapse=", "), sep="\n"))
 			}
 			obj$samples[,group] = factor( as.vector(obj$samples[,group]), levels=new_order )
 			print ("sample group order changed")
 		}else {
+			if ( ! is.factor(obj$annotation[,group])){
+				obj$annotation[,group] = factor( obj$annotation[,group] )
+			}
 			print (paste( "I got what ==",what))
 			k <- length(levels(obj$annotation[,group]) )
 			err = NULL

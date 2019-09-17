8c906fe5249c594d669c6a23094453b65adfcd61
stefanlang <Stefan.Lang@med.lu.se>
2019-09-17 13:54:03 +0200

    More clear documentation

--- R/getGenesExpressedHigherThanExpected.R
+++ R/getGenesExpressedHigherThanExpected.R
@@ -1,7 +1,18 @@
-#' After excessive clustering and really good grouping of the cells this function can be apllied to single cells dataset
-#' to identify genes that are expressed to a higher level than what the correlation to the UMI count can explain.
+#' To identify variable genes is almost the first thing that every single cell analysis strategy does.
+#' The existing VarGene function identify many highly expressed genes like 
+#' ribosomal genes or mitochondrial genes if they have not been removed before.
 #' 
-#' This strategy is hily questionable, but results in quite interesting gene lists. No normalization is required for this function to work.
+#' This normally produces problems in the downstream analysis as they normally correlate with the cell cycle which
+#' most of the times is not the main interesting cellular process.
+#' 
+#' Here we try to identify genes that are correlated less to the UMI count than would be expected.
+#' The function estimates the normal correlation values for each gene count and selects for genes that
+#' are correlated to the UMI count less than other genes with the same fraction of cells expressing them.
+#' 
+#' This has proven to especially pick genes at the lower expression range like 
+#' chromatin remodeling proteins or transcription factors.
+#' 
+#' No normalization is required for this function to work.
 #' It is always applied to the not normalized data.
 #' 
 #' @name getGenesExpressedHigherThanExpected
